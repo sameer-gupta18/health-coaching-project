ade899ecb8fbaba7e621c290b8b82bcc76259c40
sameer-gupta18 <gamingwithsam0@gmail.com>
2025-02-05 23:48:45 +0530

    final commit

--- src/AdminSide/macCheck.js
+++ src/AdminSide/macCheck.js
@@ -1,23 +1,25 @@
-import axios from "axios";
+// import axios from "axios";
 
-let access = null;
-let message = "";
+// let access = null;
+// let message = "";
 
 const verifyMAC = async () => {
-  try {
-    const response = await axios.get("http://localhost:5000/check-mac");
-    if (response.data.success) {
-      access = true;
-      message = "Access Granted";
-    } else {
-      access = false;
-      message = "You do not have permission to access this webpage";
-    }
-  } catch (error) {
-    access = false;
-    message = "ERROR 403";
-  }
+  // try {
+  //   const response = await axios.get("http://localhost:5000/check-mac");
+  //   if (response.data.success) {
+  //     access = true;
+  //     message = "Access Granted";
+  //   } else {
+  //     access = false;
+  //     message = "You do not have permission to access this webpage";
+  //   }
+  // } catch (error) {
+  //   access = false;
+  //   message = "ERROR 403";
+  // }
 
-  return access;
+  // return access;
+
+  return true
 };
 export default verifyMAC;
